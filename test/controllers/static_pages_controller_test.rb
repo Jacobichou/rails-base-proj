208a1703f1616358991c88e633bd9844a680bdf8
Jacob <jacob.pagano@gmail.com>
2015-12-24 10:29:45 -0600

    basic testing

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,19 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home pants"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Fart pants"
+  end
+
+  test "should get about page" do
+      get :about
+      assert_response :success
+      assert_select "title", "About deez nuts pants"
   end
 
 end
