111cc5d51665d54ec9a4eade08f19130049b577f
Jacob <jacob.pagano@gmail.com>
2015-12-25 09:22:09 -0600

    better test setup

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,22 +1,26 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+    def setup
+        @base_title = "Pewp |"
+    end
+
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home pants"
+    assert_select "title", "#{@base_title} Home pants"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Fart pants"
+    assert_select "title", "#{@base_title} Fart pants"
   end
 
   test "should get about page" do
       get :about
       assert_response :success
-      assert_select "title", "About deez nuts pants"
+      assert_select "title", "#{@base_title} About deez nuts pants"
   end
 
 end
